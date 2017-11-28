c9a2b40d51537a56c3db0ef4980abfb6740cc054
Daniel Duarte <danieldd.ar@gmail.com>
2017-11-28 09:56:06 -0300

    Added depends for data installer.

--- appdata/app/code/Tudai/CuentaCorriente/Setup/InstallData.php
+++ appdata/app/code/Tudai/CuentaCorriente/Setup/InstallData.php
@@ -5,10 +5,37 @@ namespace Tudai\CuentaCorriente\Setup;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 
 
 class InstallData implements InstallDataInterface
 {
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * Init
+     *
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
     /**
      * {@inheritdoc}
      */
