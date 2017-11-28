7cd352b5061d6866953dfed1bfc18949d2a11ade
Daniel Duarte <danieldd.ar@gmail.com>
2017-11-28 11:11:09 -0300

    Created upgrade.

---
+++ appdata/app/code/Tudai/CuentaCorriente/Setup/UpgradeData.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tudai\CuentaCorriente\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Customer\Model\Customer;
+
+
+/**
+ * Upgrade Data script
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * Init
+     *
+     * @param CategorySetupFactory $categorySetupFactory
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    )
+    {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if ($context->getVersion()
+            && version_compare($context->getVersion(), '1.1.0') < 0
+        ) {
+            /**
+             * @var CustomerSetup $customerSetup
+             */
+            $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+            $customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+            $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
+            /**
+             * @var $attributeSet AttributeSet
+             */
+            $attributeSet = $this->attributeSetFactory->create();
+            $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+            $customerSetup->addAttribute(Customer::ENTITY, 'enable_customer_credit', [
+                'type'         => 'int',
+                'label'        => 'Enable Customer Credit',
+                'input'        => 'boolean',
+                'required'     => false,
+                'visible'      => true,
+                'user_defined' => true,
+                'position'     => 210,
+                'system'       => 0
+            ]);
+
+            $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'enable_customer_credit')
+                ->addData([
+                    'attribute_set_id'   => $attributeSetId,
+                    'attribute_group_id' => $attributeGroupId,
+                    'used_in_forms'      => ['adminhtml_customer'],//you can use other forms also ['adminhtml_customer_address', 'customer_address_edit', 'customer_register_address']
+                ]);
+
+            $attribute->save();
+        }
+    }
+}
