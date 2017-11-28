5725e43acd267ae5ab3ae354ecce317fa6b4aece
santosluciano <luchosan74@gmail.com>
2017-11-28 11:10:05 -0300

    Upgrade bbdd 1.1.0

---
+++ appdata/app/code/Tudai/CuentaCorriente/Setup/UpgradeData.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tudai\CuentaCorriente\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Model\Customer;
+
+/**
+ * Upgrade Data script
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    protected $customerSetupFactory;
+    private $attributeSetFactory;
+
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if ($context->getVersion()
+            && version_compare($context->getVersion(), '1.1.0') < 0
+        ) {
+            $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+       /**
+     * @param \Magento\Catalog\Setup\CategorySetup $categorySetup
+     * @return void
+     */     $customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+            $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+    
+            $attributeSet = $this->attributeSetFactory->create();
+            $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+    
+            $customerSetup->addAttribute(Customer::ENTITY, 'enable_customer_credit', [
+                'type' => 'int', //tipo del atributo
+                'label' => 'Enable Customer Credit', //etiqueta
+                'input' => 'boolean', //tipo del input del atributo que voy a ver
+                'required' => false, //si es requerido
+                'visible' => true, //si va a estar disponible
+                'user_defined' => true, //se pone true para poder modificarlo por consola, significa que lo hice yo
+                'position' => 210, //posicion en el form
+                'system' => 0 //siempre es igual
+            ]);
+    
+            $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'enable_customer_credit')
+            ->addData([
+                'attribute_set_id'   => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms'      => ['adminhtml_customer'],//you can use other forms also ['adminhtml_customer_address', 'customer_address_edit', 'customer_register_address']
+            ]);
+            
+            $attribute->save();    
+        }
+    }    
+}
