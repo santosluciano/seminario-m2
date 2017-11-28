5bd27c82df2f5a6ba93202bd9c82355046ac2a05
Daniel Duarte <danieldd.ar@gmail.com>
2017-11-28 03:30:05 -0300

    Created module Tudai/CuentaCorriente with custom Customer attribute.

---
+++ appdata/app/code/Tudai/CuentaCorriente/Setup/InstallData.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tudai\CuentaCorriente\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * Init
+     *
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
+    public function __construct(
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /**
+         * @var CustomerSetup $customerSetup
+         */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
+        /**
+         * @var $attributeSet AttributeSet
+         */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+        $customerSetup->addAttribute(Customer::ENTITY, 'enable_customer_credit', [
+            'type'         => 'int',
+            'label'        => 'Enable Customer Credit',
+            'input'        => 'boolean',
+            'required'     => false,
+            'visible'      => true,
+            'user_defined' => true,
+            'position'     => 210,
+            'system'       => 0
+        ]);
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'enable_customer_credit')
+            ->addData([
+                'attribute_set_id'   => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms'      => ['adminhtml_customer'],//you can use other forms also ['adminhtml_customer_address', 'customer_address_edit', 'customer_register_address']
+            ]);
+        $attribute->save();
+    }
+}
